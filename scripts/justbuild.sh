8d552c6d7b76fa4a7a1d1e4246bc448675f0733e
Ryan Sasson <ryan@altiscale.com>
2014-10-15 11:51:58 -0700

    Editing build script for pig 13

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -ex
 ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 clean jar jar-withouthadoop
-pushd contrib/zebra
-ant -Dhadoopversion=23
-popd
 pushd contrib/piggybank/java
 ant -Dhadoopversion=23
 popd
