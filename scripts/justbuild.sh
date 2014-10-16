0e80e463107ac8176886c2cbf389e278b521d6a3
Ryan Sasson <ryan@altiscale.com>
2014-10-16 14:24:43 -0700

    Revert "removing incompatable h2 in jar name"
    This reverts commit f0c87fd82ec4f303934f3ce98a84729a3f9844ba.

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -3,7 +3,4 @@ ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dv
 pushd contrib/piggybank/java
 ant -Dhadoopversion=23
 popd
-# For oozie compatability, oozie start script doesn't expect appended "-h2"
-mv pig-${ARTIFACT_VERSION}-h2.jar pig-${ARTIFACT_VERSION}.jar
-mv pig-${ARTIFACT_VERSION}-withouthadoop-h2.jar pig-${ARTIFACT_VERSION}-withouthadoop.jar
 ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 tar
