71cba5587b5db6c9432089d1034fceed768b8567
Ravi Prakash <raviprak@altiscale.com>
2014-12-18 18:22:21 -0800

    Fixing the way to build for Hadoop2

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
-ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 clean jar jar-withouthadoop
+ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 clean jar-h12
 pushd contrib/piggybank/java
 ant -Dhadoopversion=23
 popd
-ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 tar
+ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 tar-h12
