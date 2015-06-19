db24fc9a754e8c80ebb071a2571ad3e1646cb2ee
ranjana-altiscale <ranjana@altiscale.com>
2015-06-18 19:00:09 -0700

    removed target jar-withouthadoop

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 clean jar jar-withouthadoop
+ant -Dforrest.home=/opt/apache-forrest-0.9 -Dant.home=/opt/apache-ant-1.8.4/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 clean jar 
 pushd contrib/zebra
 ant -Dhadoopversion=23
 popd
