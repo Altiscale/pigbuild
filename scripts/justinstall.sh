d9cddd885bb6e7d3dbb91074a1552956400421b7
Ravi Prakash <raviprak@altiscale.com>
2015-06-26 16:02:56 -0700

    HD-1208. Update the rpm description from Verticloud to Altiscale

--- scripts/justinstall.sh
+++ scripts/justinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
 ALTISCALE_RELEASE=${ALTISCALE_RELEASE:-0.1.0}
+RPM_DESCRIPTION="Apache Pig ${ARTIFACT_VERSION}\n\n${DESCRIPTION}"
+
 export RPM_NAME=`echo alti-pig-${ARTIFACT_VERSION}`
 echo "Building Pig Version RPM ${RPM_NAME} with RPM version ${ALTISCALE_VERSION}-${DATE_STRING}"
 
@@ -11,10 +13,10 @@ tar -xvzpf ${WORKSPACE}/pig/build/pig-${ARTIFACT_VERSION}.tar.gz
 
 cd ${RPM_DIR}
 fpm --verbose \
---maintainer ops@verticloud.com \
---vendor VertiCloud \
+--maintainer support@altiscale.com \
+--vendor Altiscale \
 --provides ${RPM_NAME} \
---description "${DESCRIPTION}" \
+--description "${RPM_DESCRIPTION}" \
 --replaces alti-pig_${ARTIFACT_VERSION} \
 -s dir \
 -t rpm \
